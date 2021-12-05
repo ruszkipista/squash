89309d3da1cc0c0e16eaa2035d5b50565abcc385
ruszkipista <oi5672@gmail.com>
2021-12-05 14:20:11 +0000

    refactor BallModel and Movable to extract MovableRectangle

--- src/de/openhpi/squash/model/BallModel.java
+++ src/de/openhpi/squash/model/BallModel.java
@@ -1,45 +1,11 @@
 package de.openhpi.squash.model;
 
-public class BallModel extends Movable implements Frameable{
+public class BallModel extends MovableRectangle{
     public float side;
 
-    private Point topLeft     = new Point(0,0);
-    private Point topRight    = new Point(0,0);
-    private Point bottomLeft  = new Point(0,0);
-    private Point bottomRight = new Point(0,0);
-    private LineSegment top = new LineSegment(topLeft, topRight);
-    private LineSegment bottom = new LineSegment(bottomLeft, bottomRight);
-    private LineSegment left = new LineSegment(topLeft, bottomLeft);
-    private LineSegment right = new LineSegment(topRight, bottomRight);
-
     public BallModel(float side){
+        super(side, side);
         this.side = side;
     }
-
-    // Movable
-    @Override
-    public void move(float lapsedTimeInSecond){
-        super.move(lapsedTimeInSecond);
-        this.setCorners();
-    }    
-
-    private void setCorners(){
-        topLeft.copyPositionAndMove(super.position, 0, 0);
-        topRight.copyPositionAndMove(super.position, +this.side, 0);
-        bottomLeft.copyPositionAndMove(super.position, 0, +this.side);
-        bottomRight.copyPositionAndMove(super.position, +this.side, +this.side);
-    }
-
-    public boolean isIntersecting(LineSegment lineSegment){
-        return top.isIntersecting(lineSegment)
-            || bottom.isIntersecting(lineSegment)
-            || left.isIntersecting(lineSegment)
-            || right.isIntersecting(lineSegment);
-    }
-
-    // Frameable
-    public void calculateNextFrame(float lapsedTimeInSec){
-        this.move(lapsedTimeInSec);
-	}
 
 }
---
+++ src/de/openhpi/squash/model/Rectangle.java
@@ -0,0 +1,12 @@
+package de.openhpi.squash.model;
+
+public abstract class Rectangle {
+    public float width;
+    public float height;
+
+    public Rectangle(float width, float height){
+        this.width = width;
+        this.height = height;
+    }
+
+}
